22daf238f9f0ecdca12e4099dbde08eb5ed932e1
subh3ndu <subhendukarmakar61@gmail.com>
2023-04-20 14:39:45 +0530

    testing

--- src/components/Stations.jsx
+++ src/components/Stations.jsx
@@ -4,7 +4,7 @@ import busData from "../data/busData";
 
 function Stations() {
   return (
-    <div className="top-[420px] p-1 absolute overscroll-y-contain -z-10">
+    <div className="absolute top-[420px] -z-10">
       {busData.map((item) => (
         <div key={item.id}>{item.stationName}</div>
       ))}
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -9,7 +9,6 @@ function Home() {
     <>
       <NavBar />
       <Map />
-      <Stations />
     </>
   );
 }
