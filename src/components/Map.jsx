7770c532c2e494f03e07d9a930001865918df5ba
subh3ndu <subhendukarmakar61@gmail.com>
2023-04-20 14:43:32 +0530

    testing

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,16 +4,18 @@ import busData from "../data/busData";
 
 export default function Map() {
   return (
-    <div className="flex flex-col fixed top-16">
-      <img src="./assets/map.jpg" alt="map" />
-      <div className="bg-[#1581C4] text-white py-2 text-sm text-center">
-        STOPS NEAR YOUR LOCATION
+    <>
+      <div className="flex flex-col fixed top-16">
+        <img src="./assets/map.jpg" alt="map" />
+        <div className="bg-[#1581C4] text-white py-2 text-sm text-center">
+          STOPS NEAR YOUR LOCATION
+        </div>
       </div>
-      <div className="absolute top-[320px] overflow-y-scroll">
+      <div className="absolute top-[420px]">
         {busData.map((item) => (
           <div key={item.id}>{item.stationName}</div>
         ))}
       </div>
-    </div>
+    </>
   );
 }
